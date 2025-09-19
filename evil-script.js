332dd2183c2325bbcc2a00ae1a9783abced01999
0x00-xian <0x00_404@proton.me>
2025-09-19 12:55:51 +0100

    enter 1st demon horde

--- evil-script.js
+++ evil-script.js
@@ -1,13 +1,20 @@
 let summon = document.getElementById("summon");
 let generator = document.getElementById("generator");
 
-let demon = '';
+let evil = document.getElementById("evil");
 
-let demons = [demon];
-let random = 0;
+let demons = [evil, evil];
+
+let random = Math.ceil(Math.random() * 1);
 
 function summonDemon() {
     generator.innerHTML = demons[random].innerHTML;
+    if (generator.innerHTML === evil.innerHTML) {
+        generator.style.fontSize = "6px";
+        generator.style.color = "#ca1717";
+        generator.style.animation = "evil 1.5s linear 0s infinite";
+    }
+    random = Math.ceil(Math.random() * 1);
 }
 
 summon.addEventListener("click", summonDemon);
