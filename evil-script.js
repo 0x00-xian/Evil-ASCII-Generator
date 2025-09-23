813378976dd94dc6e83ae07b60f0e150cf5e0abf
0x00-xian <0x00_404@proton.me>
2025-09-23 13:14:12 +0100

    add demons to generator

--- evil-script.js
+++ evil-script.js
@@ -2,19 +2,34 @@ let summon = document.getElementById("summon");
 let generator = document.getElementById("generator");
 
 let evil = document.getElementById("evil");
+let goatHead = document.getElementById("goat-head");
+let skeleton = document.getElementById("skeleton");
+let baphomet = document.getElementById("baphomet");
+let beheader = document.getElementById("beheader");
+let orc = document.getElementById("orc");
 
-let demons = [evil, evil];
+let demons = [evil, evil, goatHead, skeleton, baphomet, beheader, orc];
 
-let random = Math.ceil(Math.random() * 1);
+let random = Math.ceil(Math.random() * 6);
 
 function summonDemon() {
     generator.innerHTML = demons[random].innerHTML;
-    if (generator.innerHTML === evil.innerHTML) {
+    if (generator.innerHTML === evil.innerHTML || 
+        generator.innerHTML === goatHead.innerHTML || 
+        generator.innerHTML === baphomet.innerHTML || 
+        generator.innerHTML === orc.innerHTML) {
         generator.style.fontSize = "6px";
         generator.style.color = "#ca1717";
         generator.style.animation = "evil 1.5s linear 0s infinite";
+    } else if (generator.innerHTML === skeleton.innerHTML) {
+        generator.style.color = "#008000";
+        generator.style.animation = "skeleton 3s linear 0s infinite";
+    } else if (generator.innerHTML === beheader.innerHTML) {
+        generator.style.fontSize = "6px";
+        generator.style.color = "#ca1717";
+        generator.style.animation = "beheader 1.5s linear 0s infinite"
     }
-    random = Math.ceil(Math.random() * 1);
+    random = Math.ceil(Math.random() * 6);
 }
 
 summon.addEventListener("click", summonDemon);
