0ab482c2efae96a9d7d6e952fdd5b14ff6d6b11c
0x00-xian <0x00_404@proton.me>
2025-10-16 10:13:22 +0100

    more demons

--- js/evil-script.js
+++ js/evil-script.js
@@ -4,19 +4,28 @@ let generator = document.getElementById("generator");
 let evil = document.getElementById("evil");
 let goatHead = document.getElementById("goat-head");
 let skeleton = document.getElementById("skeleton");
+let psycho = document.getElementById("psycho");
+let slut = document.getElementById("slut");
+let gateKeeper = document.getElementById("gate-keeper");
+let hidden = document.getElementById("hidden");
+let bitcoin = document.getElementById("bitcoin");
 let baphomet = document.getElementById("baphomet");
 let warrior = document.getElementById("warrior");
 let orc = document.getElementById("orc");
 let beheader = document.getElementById("beheader");
-let gateKeeper = document.getElementById("gate-keeper");
+let spaceInvader = document.getElementById("space-invader");
+let bug = document.getElementById("bug");
 let shadowMan = document.getElementById("shadow-man");
 let jigsaw = document.getElementById("jigsaw");
 let dragon = document.getElementById("dragon");
+let doom = document.getElementById("doom");
 let witchesBrew = document.getElementById("witches-brew");
 
-let demons = [evil, evil, goatHead, skeleton, baphomet, warrior, orc, beheader, gateKeeper, bug, shadowMan, jigsaw, dragon, witchesBrew];
+let demons = [evil, evil, goatHead, skeleton, psycho, slut, 
+              hidden, bitcoin, gateKeeper, baphomet, warrior, orc, 
+              beheader, spaceInvader, bug, shadowMan, jigsaw, dragon, doom, witchesBrew];
 
-let random = Math.ceil(Math.random() * 13);
+let random = Math.ceil(Math.random() * 19);
 
 function summonDemon() {
     generator.innerHTML = demons[random].innerHTML;
@@ -28,15 +37,19 @@ function summonDemon() {
     generator.style.animation = "hover 1.5s linear 0s infinite";
     if (generator.innerHTML === evil.innerHTML) {
         generator.style.animation = "evil 6s ease-in-out 0s infinite";
+    } else if (generator.innerHTML === hidden.innerHTML) {
+        generator.style.fontSize = "10px";
+        generator.style.animation = "enter-hidden 1.5s linear 0s 1, hidden 1.5s ease-in-out 1.5s infinite";
+    } else if (generator.innerHTML === psycho.innerHTML ||
+               generator.innerHTML === gateKeeper.innerHTML) {
+        generator.style.fontSize = "7.5px";
+        generator.style.animation = "gate-keeper 1.5s linear 0s infinite";
     } else if (generator.innerHTML === baphomet.innerHTML) {
         generator.style.animation = "baphomet 1.5s linear 0s 1, hover 1.5s linear 1.5s infinite";
     } else if (generator.innerHTML === warrior.innerHTML) {
         generator.style.animation = "warrior 1.2s linear 0s infinite";
     } else if (generator.innerHTML === beheader.innerHTML) {
-        generator.style.animation = "beheader 1.5s linear 0s infinite";
-    } else if (generator.innerHTML === gateKeeper.innerHTML) {
-        generator.style.fontSize = "7.5px";
-        generator.style.animation = "beheader 1.5s linear 0s infinite";
+        generator.style.animation = "gate-keeper 1.5s linear 0s infinite";
     } else if (generator.innerHTML === jigsaw.innerHTML) {
         generator.style.animation = "jigsaw 3s linear 0s infinite";
     } else if (generator.innerHTML === dragon.innerHTML) {
@@ -44,6 +57,8 @@ function summonDemon() {
     } else if (generator.innerHTML === witchesBrew.innerHTML) {
         generator.style.fontSize = "11px";
         generator.style.animation = "witches-brew 1.5s linear 0s infinite";
+    } else if (generator.innerHTML === bitcoin.innerHTML) {
+        generator.style.color = "#ffa500";
     } else if (generator.innerHTML === skeleton.innerHTML ||
                generator.innerHTML === orc.innerHTML ||
                generator.innerHTML === bug.innerHTML ||
@@ -61,7 +76,7 @@ function summonDemon() {
             generator.style.animation = "shadow 6s linear 0s infinite";
         }
     }
-    random = Math.ceil(Math.random() * 13);
+    random = Math.ceil(Math.random() * 19);
 }
 
 summon.addEventListener("click", summonDemon);
